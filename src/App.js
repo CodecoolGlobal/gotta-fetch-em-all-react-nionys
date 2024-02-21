903d387fbcbe8b782a46f0d210aa6b031c19e1a3
Attila112 <gonda.attila112@gmail.com>
2024-02-21 10:09:58 +0100

    first page implement

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Cities from "./components/cityLocations";
+import Areas from "./components/areas";
 
 function App() {
+  const [locations, setLocations] = useState([]);
+  const [linkAreas, setLinkLocations] = useState(null);
+  const [pokemons, setPokemons] = useState(null);
+ //const [linkPokemons, setLinkPokemons] = useState(null)
+  //const [areas, setAreas] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://pokeapi.co/api/v2/location");
+      const data = await response.json();
+      setLocations(data.results);
+    }
+    fetchData();
+  }, []);
+
+  console.log(pokemons);
   return (
     <div className="App">
+      {linkAreas ? (
+        <Areas url={linkAreas}
+       setPokemons={setPokemons} />
+      ) : (
+        locations.map((city, index) => (
+          <Cities
+            url={city.url}
+            index={index}
+            name={city.name}
+            click={() => setLinkLocations(city.url)}
+          />
+        ))
+      )}
     </div>
   );
 }
---
+++ src/components/areas.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+function Areas(props) {
+  const [areas, setAreas] = useState([]);
+  const [linkPokemons, setLinkPokemons] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(props.url);
+      const data = await response.json();
+      setAreas(data.areas);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(linkPokemons);
+      const data = await response.json();
+      props.setPokemons(data.pokemon_encounters);
+    }
+    fetchData();
+  }, [linkPokemons]);
+
+  return (
+    <div>
+      {areas.map((area) => (
+        <div className="areaBox" onClick={() => setLinkPokemons(area.url)}>
+          <h1>{area.name}</h1>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Areas;
