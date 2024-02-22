a7e4967f3003218f0b6d5dd227ecff9b188fd7dd
Samu <108835512+nionys@users.noreply.github.com>
2024-02-22 11:58:29 +0100

    flip myPokemon

--- src/components/BattlePokemon.jsx
+++ src/components/BattlePokemon.jsx
@@ -2,9 +2,10 @@ import HealthBar from "./HealthBar";
 
 
 export default function BattlePokemon(props) {
+  const flipped = props.className === "myPokemon";
   return <div className={props.className}>
     <h2>{props.pokemonObject.name}</h2>
-    <img height="200" src={props.pokemonObject.images.front} alt=""></img>
+    <img style={{transform: `scaleX(${flipped? -1 : 1})`}}height="200" src={props.pokemonObject.images.front} alt=""></img>
     <h2>{props.pokemonObject.hp}/{props.pokemonObject.maxHp}</h2>
     <HealthBar pokemonObject={props.pokemonObject}></HealthBar>
   </div>
