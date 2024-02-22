0d277afc7a22a9f3715c6cd8f43391bd584b5e7f
Attila112 <gonda.attila112@gmail.com>
2024-02-22 13:14:20 +0100

    first page styles

--- src/components/Pokeimg.jsx
+++ src/components/Pokeimg.jsx
@@ -1,24 +1,12 @@
-import React, { useEffect, useState } from "react";
-
-const fetchPokemon = async (link) => {
-  const response = await fetch(link);
-  const pokemon = await response.json();
-  return pokemon;
-};
-
 function PokeImage(props) {
-  const [pokemon, setPokemon] = useState(null);
-  useEffect(() => {
-    fetchPokemon(props.link).then((data) => setPokemon(data));
-  }, []);
-
-  return (
-    <img
+  const pokemon = props.pokemon;
+  return <div> <img
       src={pokemon?.sprites.other.showdown[props.direction]}
       alt={pokemon?.name}
-      onClick={()=>{props.setSelectedPokemon(pokemon)}}
-    ></img>
-  );
+      onClick={()=>{props.setSelectedPokemon(pokemon)}}/>
+      <h4>{pokemon?.name}</h4>
+    </div>
+
 }
 
 export default PokeImage;
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,28 +1,37 @@
 import React from "react";
 import PokeImage from "./Pokeimg";
+import NotFound from "./NotFound";
 
 function Pokemon(props) {
-  console.log(props.encounter);
-  const usersPokemon = [
-    "https://pokeapi.co/api/v2/pokemon/bulbasaur",
-    "https://pokeapi.co/api/v2/pokemon/charizard",
-    "https://pokeapi.co/api/v2/pokemon/poliwhirl",
-  ];
-  const areaPokémon = props.encounter;
+  const areaPokemons = props.areaPokemons;
+  const userPokemons = props.userPokemons;
   const setSelectedUserPokemon = props.setSelectedUserPokemon;
   const setSelectedAreaPokemon = props.setSelectedAreaPokemon;
   return (
-    <div>
-        <div className="userPokémon">
-      {usersPokemon.map((link, index) => (
-        <PokeImage key={index} link={link} direction='back_default' setSelectedPokemon={setSelectedUserPokemon} />
-      ))}
-        </div>
-        <div className="areaPokémon">
-        {areaPokémon&&areaPokémon.map((pokemon, index) => (
-        <PokeImage key={index} link={pokemon.pokemon.url} direction='front_default' setSelectedPokemon={setSelectedAreaPokemon} />
-      ))}
-        </div>
+    <div className="pokemons">
+      <div className="userPokémon">
+        <h3>Your Pokemons</h3>
+        {userPokemons.map((pokemon, index) => (
+          <PokeImage
+            key={index}
+            pokemon={pokemon}
+            direction="front_default"
+            setSelectedPokemon={setSelectedUserPokemon}
+          />
+        ))}
+      </div>
+      <div className="areaPokémon">
+        <h3>Enemy Pokemons</h3>
+        {areaPokemons ?
+          areaPokemons.map((pokemon, index) => (
+            <PokeImage
+              key={index}
+              pokemon={pokemon}
+              direction="front_default"
+              setSelectedPokemon={setSelectedAreaPokemon}
+            />
+          )): <NotFound/>}
+      </div>
     </div>
   );
 }
